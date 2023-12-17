b46e1ffe5f7c7a47c2c5dff1f5612df7da5f70dc
ShauryaSaha <153977979+ShauryaSaha@users.noreply.github.com>
2023-12-17 10:37:40 +0530

    added kbc.py

---
+++ kbc.py
@@ -0,0 +1,42 @@
+
+print("lvl 1")
+print("What is the capital of india ?")
+print("Your options are - ")
+
+Options = ["1.jharkhand,delhi , bihar, kerela"]
+print(Options)
+correct_answer = input("Enter your answer - ")
+if correct_answer == "delhi":
+    print("Yes it is the correct answer; ", "You have  won 10,000" )
+    print("Sorry! it is the wrong answer")
+
+
+
+print("lvl 2")
+print("Name the national bird of india?")
+print("Your options are - ")
+
+Options = ["crow , peacock , duck , saktiman"]
+print(Options)
+correct_answer = input("Enter your answer - ")
+if correct_answer == "peacock":
+    print("Yes it is the correct answer; ", "You have  won 20,000" )
+else:
+    print("Sorry! it is the wrong answer")
+
+
+
+print("lvl 3")
+print("Who is the current PM of india ?")
+print("Your options are - ")
+
+Options = ["shaurya , Narendra Modi , harshit , shaktiman"]
+print(Options)
+correct_answer = input("Enter your answer - ")
+if correct_answer == "Narendra Modi":
+    print("Yes it is the correct answer; ", "You have  won 30,000" )
+else:
+    print("Sorry! it is the wrong answer")
+
+
+
